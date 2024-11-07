733e07ad552a1e729026371b08e912eb022a1a12
itzzmerov <balingbing.johnrovie20@gmail.com>
2024-11-07 18:40:20 +0800

    Update NavBar.jsx

--- src/components/LandingPage/NavBar/NavBar.jsx
+++ src/components/LandingPage/NavBar/NavBar.jsx
@@ -6,14 +6,14 @@ const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="bg-[#61AD4E] text-white p-4 flex justify-between items-center">
-            <img src={logo} alt="Logo" className="w-12 h-12" />
-            <div className="hidden md:flex space-x-6">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
-                <a href="#login">Login</a>
+        <nav className="bg-[#61AD4E] text-white py-4 px-10 flex justify-between items-center">
+            <img src={logo} alt="Logo" className="w-24 h-24" />
+            <div className="hidden md:flex space-x-6 text-[18px]">
+                <a className="hover:text-black" href="#home">Home</a>
+                <a className="hover:text-black" href="#about">About</a>
+                <a className="hover:text-black" href="#services">Services</a>
+                <a className="hover:text-black" href="#contact">Contact</a>
+                <a className="hover:text-black" href="#login">Login</a>
             </div>
             <button
                 className="md:hidden focus:outline-none text-2xl"
@@ -23,11 +23,11 @@ const NavBar = () => {
             </button>
             {isOpen && (
                 <div className="absolute top-16 left-0 w-full bg-[#181818] md:hidden flex flex-col items-center space-y-4 py-4">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
-                <a href="#login">Login</a>
+                    <a className="hover:text-[#61AD4E]" href="#home">Home</a>
+                    <a className="hover:text-[#61AD4E]" href="#about">About</a>
+                    <a className="hover:text-[#61AD4E]" href="#services">Services</a>
+                    <a className="hover:text-[#61AD4E]" href="#contact">Contact</a>
+                    <a className="hover:text-[#61AD4E]" href="#login">Login</a>
                 </div>
             )}
         </nav>
