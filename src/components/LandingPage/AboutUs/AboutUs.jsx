06ecd7e6d2faa8a8f46d35193f2a5242040599a1
John Rovie R. Balingbing <balingbing.johnrovie20@gmail.com>
2024-11-08 13:01:47 +0800

    Update AboutUs.jsx

--- src/components/LandingPage/AboutUs/AboutUs.jsx
+++ src/components/LandingPage/AboutUs/AboutUs.jsx
@@ -9,9 +9,9 @@ const AboutUs = () => {
                 <img src={logo} alt="About Logo" className="w-full md:w-[40%] mb-4 md:mb-0 md:mr-6" />
                 <div className='w-full lg:w-[50%] flex flex-col items-center md:items-start gap-5'>
                     <h2 className="text-[24px] lg:text-[32px] font-bold font-Montserrat">ABOUT US</h2>
-                    <h2 className="text-[32px] lg:text-[72px] lg:leading-[85px] text-center font-bold font-Montserrat">IPAMALIHOG KAY LOLO</h2>
-                    <p className='lg:text-[17px] text-center'>We are a trusted delivery service provider based in Mati City, Davao Oriental. We specialize in bringing essentials and goods right to your doorstep with a focus on convenience, reliability, and community care.</p>
-                    <button className="mt-4 border-darkBlack border-[3px] hover:bg-darkBlack hover:text-lightWhite w-[90%] md:w-[60%] lg:w-[40%] px-6 py-2 rounded-full font-Montserrat text-[18px] font-semibold  flex items-center justify-center gap-1"><MenuBookIcon /> Learn More</button>
+                    <h2 className="text-[32px] lg:text-[72px] lg:leading-[85px] text-center md:text-left font-bold font-Montserrat">IPAMALIHOG KAY LOLO</h2>
+                    <p className='lg:text-[17px] text-center md:text-left'>We are a trusted delivery service provider based in Mati City, Davao Oriental. We specialize in bringing essentials and goods right to your doorstep with a focus on convenience, reliability, and community care.</p>
+                    <button className="mt-4 border-darkBlack border-[3px] hover:bg-darkBlack hover:text-lightWhite w-[90%] md:w-[70%] lg:w-[50%] px-6 py-2 rounded-full font-Montserrat text-[18px] font-semibold  flex items-center justify-center gap-1"><MenuBookIcon /> Learn More</button>
                 </div>
             </div>
         </section>
