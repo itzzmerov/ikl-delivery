1e847c9d6c7428239214e31d82490a4880ead773
itzzmerov <balingbing.johnrovie20@gmail.com>
2025-01-27 14:12:22 +0800

    Update TopNavbar.jsx

--- src/components/RidersDashboard/TopNavbar/TopNavbar.jsx
+++ src/components/RidersDashboard/TopNavbar/TopNavbar.jsx
@@ -44,7 +44,6 @@ const TopNavbar = ({ toggleSidebar }) => {
                 </button>
             </div>
             <div className="flex items-center gap-2 px-3">
-                <br />
                 {/* <div className='flex flex-row'>
                     <input
                         type="text"
