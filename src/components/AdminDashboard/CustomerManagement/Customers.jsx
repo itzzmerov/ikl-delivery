a7be1d90e26df4a218010cc99e7b94620af8a19d
Sir Rov <balingbing.johnrovie20@gmail.com>
2025-04-01 14:10:42 +0800

    Update Customers.jsx

--- src/components/AdminDashboard/CustomerManagement/Customers.jsx
+++ src/components/AdminDashboard/CustomerManagement/Customers.jsx
@@ -49,13 +49,13 @@ const Customers = () => {
     }
 
     return (
-        <div className="p-8 flex-1">
+        <div className="p-8 h-[100vh] flex-1">
             <div className='flex justify-between items-center mb-2'>
                 <h1 className="text-2xl font-semibold mb-4">Customers List</h1>
                 <button onClick={openCustomerForm} className='bg-darkBlack p-2 text-lightWhite hover:bg-lightBlack'><AddIcon /> Add New Customer</button>
             </div>
 
-            <div className="min-w-full h-96 overflow-x-auto overflow-y-auto">
+            <div className="min-w-full h-[80%] overflow-x-auto overflow-y-auto">
                 <table className="min-w-full bg-lightWhite border border-gray-200">
                     <thead className="bg-gray-800 text-lightWhite sticky top-0">
                         <tr className="text-left">
