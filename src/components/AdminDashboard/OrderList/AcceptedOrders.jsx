1908b57ad4d235faa48801a29cbd978f03b822fb
Sir Rov <balingbing.johnrovie20@gmail.com>
2025-04-01 14:07:29 +0800

    Update AcceptedOrders.jsx

--- src/components/AdminDashboard/OrderList/AcceptedOrders.jsx
+++ src/components/AdminDashboard/OrderList/AcceptedOrders.jsx
@@ -166,7 +166,7 @@ const AcceptedOrders = () => {
     };
 
     return (
-        <div className="p-8 flex-1">
+        <div className="p-8 h-[100vh] flex-1">
             <div className='flex justify-between items-center mb-2'>
                 <h1 className="text-2xl font-semibold mb-4">Order List</h1>
                 <div className="flex gap-2">
@@ -191,7 +191,7 @@ const AcceptedOrders = () => {
                 </div>
             </div>
 
-            <div className="min-w-full h-96 overflow-x-auto overflow-y-auto">
+            <div className="min-w-full h-[80%] overflow-x-auto overflow-y-auto">
                 <table className="min-w-full bg-lightWhite border border-gray-200">
                     <thead className="bg-gray-800 text-lightWhite sticky top-0">
                         <tr className="text-left">
