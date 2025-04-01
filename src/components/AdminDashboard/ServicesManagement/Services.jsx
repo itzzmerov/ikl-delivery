f7f8450f51043abb1853af18f8acf7bec99973c5
Sir Rov <balingbing.johnrovie20@gmail.com>
2025-04-01 14:09:29 +0800

    Updated Table

--- src/components/AdminDashboard/ServicesManagement/Services.jsx
+++ src/components/AdminDashboard/ServicesManagement/Services.jsx
@@ -49,7 +49,7 @@ const Services = () => {
     };
 
     return (
-        <div className="p-8 flex-1">
+        <div className="p-8 h-[100vh] flex-1">
             <div className='flex justify-between items-center mb-2'>
                 <h1 className="text-2xl font-semibold mb-4">Services List</h1>
                 <button onClick={openServiceForm} className='bg-darkBlack p-2 text-lightWhite hover:bg-lightBlack'>
@@ -58,7 +58,7 @@ const Services = () => {
             </div>
 
             {/* Scrollable container */}
-            <div className="min-w-full h-96 overflow-x-auto overflow-y-auto">
+            <div className="min-w-full h-[80%] overflow-x-auto overflow-y-auto">
                 <table className="min-w-full bg-lightWhite border border-gray-200">
                     <thead className="bg-gray-800 text-lightWhite sticky top-0">
                         <tr className="text-left">
