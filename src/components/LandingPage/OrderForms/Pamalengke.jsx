852c53c5ef2ab92f84daf1ea132bd0af265913cd
John Rovie R. Balingbing <balingbing.johnrovie20@gmail.com>
2025-04-13 19:15:12 +0800

    Update Pamalengke.jsx

--- src/components/LandingPage/OrderForms/Pamalengke.jsx
+++ src/components/LandingPage/OrderForms/Pamalengke.jsx
@@ -86,9 +86,16 @@ const Pamalengke = ({ onClose }) => {
                 createdAt: new Date().toISOString(),
                 basePrice,
             };
-
+ 
             const result = await addDoc(collection(db, 'orders'), pamalengkeData);
             console.log('Pamalengke request created with ID:', result.id);
+
+            const notificationMessage = `${formData.customerFirstName} ${formData.customerLastName} has placed a new ${formData.service} order.`;
+            await addDoc(collection(db, 'notifications'), {
+                message: notificationMessage,
+                timestamp: new Date(),
+                status: "unread",
+            });
 
             setShowPopup(true);
 
