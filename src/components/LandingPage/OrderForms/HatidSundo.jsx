9b086a4f62420642688142ab574f11a44e7052d6
John Rovie R. Balingbing <balingbing.johnrovie20@gmail.com>
2025-04-13 19:12:19 +0800

    Update HatidSundo.jsx

--- src/components/LandingPage/OrderForms/HatidSundo.jsx
+++ src/components/LandingPage/OrderForms/HatidSundo.jsx
@@ -19,7 +19,7 @@ const HatidSundo = ({ onClose }) => {
         pickupTime: '',
         specialRequests: '',
     });
-
+ 
     useEffect(() => {
         const fetchUserData = async () => {
             if (!currentUser) return;
@@ -88,6 +88,13 @@ const HatidSundo = ({ onClose }) => {
 
             const result = await addDoc(collection(db, 'orders'), hatidSundoData);
             console.log('Hatid Sundo request created with ID:', result.id);
+
+            const notificationMessage = `${formData.customerFirstName} ${formData.customerLastName} has placed a new ${formData.service} order.`;
+            await addDoc(collection(db, 'notifications'), {
+                message: notificationMessage,
+                timestamp: new Date(),
+                status: "unread",
+            });
 
             setShowPopup(true);
 
